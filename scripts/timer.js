80a672065e1e4dab20ae90a6bb4534f2bd11a120
rafmob <89050871+rafmob@users.noreply.github.com>
2022-11-04 21:50:29 -0300

    Refatoração de código

---
+++ scripts/timer.js
@@ -0,0 +1,38 @@
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+
+function resetTimer() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(timerTimeOut)
+}
+
+function countdown() {
+  timerTimeOut = setTimeout(function() {
+    let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+    
+    updateTimerDisplay(minutes, 0)
+
+    if (minutes <= 0){
+      
+      resetControls()
+      
+      return
+    }
+    
+    if(seconds <= 0){
+      seconds = 2
+
+      --minutes
+    }
+
+    updateTimerDisplay(minutes, String(seconds -1))
+
+    countdown()
+  }, 1000)
+}
+
+// Named export
+export { countdown, resetTimer }
